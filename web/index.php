42731ebbb2939e30428e0fbde6cdb627885156bc
andreacassina <33427607+andreacassina@users.noreply.github.com>
2017-12-07 17:09:16 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -13,14 +13,19 @@
 			$indirizzo = "https://api.foursquare.com/v2/venues/search?client_id=$client_id&client_secret=$client_secret&query=$query&v=20170801";
 			
 			
+			/*	
 			// Get cURL resource
 			$curl = curl_init();
 			// Set some options - we are passing in a useragent too here
-			curl_setopt_array($curl, array(CURLOPT_RETURNTRANSFER => 1,CURLOPT_URL => $indirizzo,CURLOPT_USERAGENT => 'Codular Sample cURL Request'));
+			curl_setopt_array($curl, array(CURLOPT_RETURNTRANSFER => 1,CURLOPT_URL => $indirizzo));
 			// Send the request & save response to $resp
 			$resp = curl_exec($curl);
 			// Close request to clear up some resources
 			curl_close($curl);
+		*/
+		$curl = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$indirizzo);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 				
 			echo "$resp";
 		?>
